21d3cc9915780e2c063440be7f32b60d1a337695
awseng19 <maryam.r10843@gmail.com>
2025-02-02 20:17:55 -0500

    Update resourcegroup.tf

--- resourcegroup.tf
+++ resourcegroup.tf
@@ -1,16 +1,16 @@
 resource "azurerm_resource_group" "mcit420zz5um" {
-  name     = "mcit_resource_group_w24"
+  name     = "mcit_resource_group_22maryam"
   location = "canadacentral"
 }
 resource "azurerm_resource_group" "mcit420zz5um2" {
-  name     = "mcit_resource_group_w24_2"
+  name     = "mcit_resource_group_w23maryam"
   location = "canadacentral"
 }
 resource "azurerm_resource_group" "mcit420zz5um3" {
-  name     = "mcit_resource_group_w24_3"
+  name     = "mcit_resource_group_w24maryam"
   location = "canadacentral"
 }
 resource "azurerm_resource_group" "mcit420zz5um4" {
-  name     = "mcit_resource_group_w24_4"
+  name     = "mcit_resource_group_w25maryam"
   location = "canadacentral"
 }
