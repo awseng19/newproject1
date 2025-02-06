2fb555a2a7ba1217fe29c0994f27655ef66aa38f
awseng19 <maryam.r10843@gmail.com>
2025-02-05 20:39:10 -0500

    Update app_function.tf

--- app_function.tf
+++ app_function.tf
@@ -10,8 +10,8 @@ resource "azurerm_function_app" "function1" {
   resource_group_name        = azurerm_resource_group.mcit420zz5um4.name
   location                   = azurerm_resource_group.mcit420zz5um4.location
   storage_account_name       = azurerm_storage_account.mcitstorageaccount.name
-  storage_account_access_key = azurerm_storage_account.example.primary_access_key
-  service_plan_id            = azurerm_service_plan.example.id
+  storage_account_access_key = azurerm_storage_account.mcitstorageaccount.primary_access_key
+  service_plan_id            = azurerm_service_plan.fun1.id
   os_type                    = "linux"
 
   site_config {
