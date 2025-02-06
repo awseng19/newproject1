e803b3e17f63080436db799c59f1d485704a0dce
awseng19 <maryam.r10843@gmail.com>
2025-02-05 20:37:24 -0500

    Update app_function.tf

--- app_function.tf
+++ app_function.tf
@@ -1,14 +1,14 @@
-resource "azurerm_service_plan" "example" {
+resource "azurerm_service_plan" "fun1" {
   name                = "example-service-plan"
-  resource_group_name = azurerm_resource_group.example.name
-  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.mcit420zz5um4.name
+  location            = azurerm_resource_group.mcit420zz5um4.location
   os_type             = "Linux"
   sku_name            = "Y1"  # Consumption Plan
 }
-resource "azurerm_function_app" "example" {
+resource "azurerm_function_app" "function1" {
   name                       = "yourfirstnamemcitfunction"
-  resource_group_name        = azurerm_resource_group.example.name
-  location                   = azurerm_resource_group.example.location
+  resource_group_name        = azurerm_resource_group.mcit420zz5um4.name
+  location                   = azurerm_resource_group.mcit420zz5um4.location
   storage_account_name       = azurerm_storage_account.example.name
   storage_account_access_key = azurerm_storage_account.example.primary_access_key
   service_plan_id            = azurerm_service_plan.example.id
