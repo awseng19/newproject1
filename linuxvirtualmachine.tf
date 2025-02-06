03ce7997d021667abe78c49bfaa56413c7be601d
awseng19 <maryam.r10843@gmail.com>
2025-02-05 20:27:45 -0500

    Update linuxvirtualmachine.tf

--- linuxvirtualmachine.tf
+++ linuxvirtualmachine.tf
@@ -1,1 +1,36 @@
+resource "azurerm_virtual_network" "maryam" {
+  name                = "maryam-network"
+  address_space       = ["10.0.0.0/16"]
+  location            = azurerm_resource_group.mcit420zz5um.location
+  resource_group_name = azurerm_resource_group.mcit420zz5um.name
+}
+
+resource "azurerm_subnet" "maryam" {
+  name                 = "maryam1"
+  resource_group_name  = azurerm_resource_group.mcit420zz5um.name
+  virtual_network_name = azurerm_virtual_network.maryam.name
+  address_prefixes     = ["10.0.2.0/24"]
+}
+
+resource "azurerm_network_interface" "maryaminterface" {
+  name                = "maryam-nic"
+  location            = azurerm_resource_group.mcit420zz5um.location
+  resource_group_name = azurerm_resource_group.mcit420zz5um.name
+
+  ip_configuration {
+    name                          = "maryam123"
+    subnet_id                     = azurerm_subnet.maryam.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
+
+resource "azurerm_linux_virtual_machine" "example" {
+  name                = "example-machine"
+  resource_group_name = azurerm_resource_group.mcit420zz5um.name
+  location            = azurerm_resource_group.mcit420zz5um.location
+  size                = "Standard_F2"
+  admin_username      = "adminuser"
+  network_interface_ids = [
+    azurerm_network_interface.maryaminterface.id,
+  ]
 
