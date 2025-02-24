5e69ea0050aba0d10695ef7a45c3583cba62ba2c
awseng19 <maryam.r10843@gmail.com>
2025-02-23 22:26:56 -0500

    Update linuxmd1.tf

--- linuxmd1.tf
+++ linuxmd1.tf
@@ -6,19 +6,19 @@ module "linuxservers" {
     vnet_subnet_id      = "${module.vnet.vnet_subnets[0]}"
   }
  
-  module "windowsservers" {
-    source              = "Azure/vm/azurerm"
-    location            = "West US 2"
-    vm_hostname         = "mywinvm" // line can be removed if only one VM module per resource group
-    admin_password      = "ComplxP@ssw0rd!"
-    vm_os_simple        = "WindowsServer"
-    public_ip_dns       = ["winsimplevmips"] // change to a unique name per datacenter region
-    vnet_subnet_id      = "${module.vnet.vnet_subnets[0]}"
-  }
+ # module "windowsservers" {
+ #   source              = "Azure/vm/azurerm"
+ #   location            = "West US 2"
+ #   vm_hostname         = "mywinvm" // line can be removed if only one VM module per resource group
+ #   admin_password      = "ComplxP@ssw0rd!"
+ #   vm_os_simple        = "WindowsServer"
+ #   public_ip_dns       = ["winsimplevmips"] // change to a unique name per datacenter region
+ #   vnet_subnet_id      = "${module.vnet.vnet_subnets[0]}"
+ # }
  
-  module "vnet" {
-    source              = "Azure/vnet/azurerm"
-    version             = "~> 1.0.0"
-    location            = "West US 2"
-    resource_group_name = "terraform-vm"
-  }
+ # module "vnet" {
+ #   source              = "Azure/vnet/azurerm"
+ #   version             = "~> 1.0.0"
+ #   location            = "West US 2"
+  #  resource_group_name = "terraform-vm"
+ # }
